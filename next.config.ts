e62f73a8b5c8d56f4c62d35b1eb930fd4801df9a
javedwasim <javedafaq@gmail.com>
2026-08-08 03:43:14 +0500

    complete profile

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: "export",
+const nextConfig: NextConfig = {  
   basePath: "/portfolio",
   assetPrefix: "/portfolio/",
   images: {
     unoptimized: true,
   },
 };
-
 export default nextConfig;
